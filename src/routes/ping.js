ecd16f08b0edeaf333aa5941f9abb3167d836e1b
marcos <marcosvalens@gmail.com>
2020-03-24 19:13:04 +0100

    Marcos - Ping Routes done
    Signed-off-by: marcos <marcosvalens@gmail.com>

---
+++ src/routes/ping.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const {pinger, utils} = require('@rochismo/net-utils');
+
+async function pingSweep() {
+    const details = await utils.getDetails();
+    let ip = details.cidr;
+    ip = ip.split(",");
+    ip = ip[0];
+    let aliveHosts;
+    aliveHosts = await pinger.pingSweep(ip);
+    return aliveHosts;
+}
+
+pingSweep().then(function (data) {
+    console.log(data)
+});
+
+
+router.get("/", (req, res) => {
+    async function pingToSelf() {
+        const details = await utils.getDetails();
+        let ip = details.ip_cidr;
+        ip = ip.split("/");
+        ip = ip + "'";
+        ip = ip[0];
+        return await pinger.ping(ip)
+    }
+});
+
+router.get("/pingSweep", (req, res) => {
+    async function pingSweep() {
+        const details = await utils.getDetails();
+        let ip = details.cidr;
+        ip = ip.split(",");
+        ip = ip[0];
+        let aliveHosts;
+        aliveHosts = await pinger.pingSweep(ip);
+        return aliveHosts;
+    }
+});
+
+
+module.exports = router;
