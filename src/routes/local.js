173f26cd21e1197ca82e7873eeb2889223228563
unknown <alexis.platino@gmail.com>
2020-05-21 16:45:42 +0200

    Added basic implementation

--- src/routes/local.js
+++ src/routes/local.js
@@ -1,4 +1,9 @@
 const router = require("express").Router;
+const {utils} = require("@rochismo/net-utils");
 
+router.get("/gateway", async (req, res) => {
+    const interface = await utils.getInterface();
+    res.status(200).json(interface)
+})
 
 module.exports = router;
