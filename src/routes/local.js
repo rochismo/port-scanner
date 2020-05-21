4b6c473a74ead6a3e162f9806106525ebe655341
unknown <alexis.platino@gmail.com>
2020-05-21 16:51:06 +0200

    We now return the gateway

--- src/routes/local.js
+++ src/routes/local.js
@@ -1,9 +1,14 @@
-const router = require("express").Router;
-const {utils} = require("@rochismo/net-utils");
+const router = require("express").Router();
+const { utils } = require("@rochismo/net-utils");
 
 router.get("/gateway", async (req, res) => {
-    const interface = await utils.getInterface();
-    res.status(200).json(interface)
-})
+    try {
+        const interface = await utils.getInterface();
+        res.status(200).json(interface.gateway_ip);
+    } catch(e) {
+        // Default interface not found, not returning anything
+        res.status(204).json("");
+    }
+});
 
 module.exports = router;
